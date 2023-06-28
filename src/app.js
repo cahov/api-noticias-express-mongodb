034169ec3f7f5a6e5b62745180de7dd4506f7ea2
Admin <christian.herrera.onaindi@gmail.com>
2023-06-28 18:47:13 -0400

    Fix bugs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const router = require("./routes/index.routes.js");
 
 //middlewares
 app.use(express.urlencoded({ extended: false }));
+// Middleware para habilitar el CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 //routes
 app.use(router);
 
